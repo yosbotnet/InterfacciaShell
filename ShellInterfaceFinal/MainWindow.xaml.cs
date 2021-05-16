efd33c1209ce4ce66392b5928c4e5423d455d025
yosbotnet <79740870+yosbotnet@users.noreply.github.com>
2021-05-16 18:37:22 +0200

    Aggiunta variabilio e btnConnect_click

--- ShellInterfaceFinal/MainWindow.xaml.cs
+++ ShellInterfaceFinal/MainWindow.xaml.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using System.Diagnostics;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -23,6 +28,36 @@ namespace ShellInterfaceFinal
         public MainWindow()
         {
             InitializeComponent();
+        }
+        Socket corrente;
+        TcpClient connect;
+        TcpListener server;
+        NetworkStream stream;
+        Process processoCmd;
+        static StreamWriter outStream;
+        static StreamReader inStream;
+        static StreamWriter toCmd;
+        static StreamReader fromCmd;
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                IPAddress ipDest = IPAddress.Parse(txtIP.Text);
+                int port = int.Parse(txtPorta.Text);
+                IPEndPoint ipEP = new IPEndPoint(ipDest, port);
+                connect = new TcpClient();
+                connect.Connect(ipEP);
+
+                stream = connect.GetStream();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Errore nella connessione:" + ex.Message);
+            }
+            btnDisconnect.IsEnabled = true;
+            btnInvia.IsEnabled = true;
+            btnAscolta.IsEnabled = false;
         }
     }
 }
