d5f9136e743ceb5b50fa24ae0ffdef9ac0b77683
yosbotnet <79740870+yosbotnet@users.noreply.github.com>
2021-05-16 18:38:58 +0200

    Aggiunto Codice bottone ascolta, esecuzione dei comandi

--- ShellInterfaceFinal/MainWindow.xaml.cs
+++ ShellInterfaceFinal/MainWindow.xaml.cs
@@ -58,6 +58,110 @@ namespace ShellInterfaceFinal
             btnDisconnect.IsEnabled = true;
             btnInvia.IsEnabled = true;
             btnAscolta.IsEnabled = false;
+
+        }
+        private void btnAscolta_Click(object sender, RoutedEventArgs e)
+        {
+            Thread t = null;
+            try
+            {
+                t = new Thread(new ParameterizedThreadStart(ThreadAscolto));
+                t.Start(txtPortaAscolto.Text);
+                btnAscolta.IsEnabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Errore!" + ex.Message);
+                btnAscolta.IsEnabled = true;
+            }
+            finally
+            {
+                t.Join();
+                if (server != null)
+                    server.Stop();
+            }
+
+        }
+        public void ThreadAscolto(object portaAscolto)
+        {
+            string _portaAscolto = (string)portaAscolto;
+            #region inizializza processo
+            //inizializzo il processo cmd
+            processoCmd = new Process();
+            processoCmd.StartInfo = new ProcessStartInfo("cmd");
+            processoCmd.StartInfo.CreateNoWindow = true;
+            processoCmd.StartInfo.UseShellExecute = true;
+            processoCmd.StartInfo.RedirectStandardOutput = true;
+            processoCmd.StartInfo.RedirectStandardInput = true;
+            processoCmd.StartInfo.RedirectStandardError = true;
+            processoCmd.StartInfo.UseShellExecute = false;
+            processoCmd.OutputDataReceived += new DataReceivedEventHandler(SendOutput);
+            server = new TcpListener(IPAddress.Any, int.Parse(_portaAscolto));
+            server.Start();
+
+            #endregion
+            //fromCmd = processoCmd.StandardOutput;
+
+
+
+            byte[] bufferC = new byte[1024];
+            String comando = null;
+            while (server.Pending() == false)
+            {
+                Thread.Sleep(50);
+            }
+            //MessageBox.Show("Exited! Connecting");
+            Socket ascolto = server.AcceptSocket();
+            processoCmd.Start();
+            toCmd = processoCmd.StandardInput;
+            processoCmd.BeginOutputReadLine();
+            bool aperto = true;
+            while (aperto)
+            {
+                //leggo il comando
+                comando = null;
+                stream = new NetworkStream(ascolto);
+                inStream = new StreamReader(stream);
+                outStream = new StreamWriter(stream);
+                toCmd.AutoFlush = true;
+                outStream.AutoFlush = true;
+                int i;
+                while ((i = stream.Read(bufferC, 0, bufferC.Length)) != 0)
+                {
+                    comando = System.Text.Encoding.ASCII.GetString(bufferC);
+                    //esegui il comando
+                    Esegui(comando);
+
+
+                }
+                Thread.Sleep(50);
+            }
+            ascolto.Close();
+
+            return;
+        }
+        private static void SendOutput(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            StringBuilder strOutput = new StringBuilder();
+
+            if (!String.IsNullOrEmpty(outLine.Data))
+            {
+                try
+                {
+                    strOutput.Append(outLine.Data);
+                    outStream.WriteLine(strOutput);
+                    outStream.Flush();
+                }
+                catch (Exception ex)
+                {
+                    // silence is golden
+                }
+            }
+        }
+        public void Esegui(string comando)
+        {
+            processoCmd.StandardInput.WriteLine(comando + "\r\n");
+
         }
     }
 }
